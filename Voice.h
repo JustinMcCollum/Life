14a2ef30dee8777bff7166eb8048012ba59686fb
JustinMcCollum <59267398+JustinMcCollum@users.noreply.github.com>
2019-12-26 17:12:42 -0600

    Add files via upload
    This is a Text to Speech program I learned to do and would love to expand on it.

---
+++ Voice.h
@@ -0,0 +1,13 @@
+#ifndef VOICE_H
+#define VOICE_H
+
+#include <iostream>
+
+using namespace std;
+
+class Voice
+{
+public:
+	void say(string phrase); //Used to textually and audibly communicate a phrase 
+};
+#endif
